be357a76fbb0bc9c90a5762e7da77b4d2f658736
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-24 11:43:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+ BBQ.each do |seasons, holiday|
+   puts ["BBQ"]
 end
 
 
